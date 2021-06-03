3eff3be56ee12f36b7d519d71eb80b4e05ca40a1
alanpacaud <alan.propacaud@gmail.com>
2021-06-03 21:13:35 +0200

    add pages and route

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>À propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio atque consequatur obcaecati esse aliquid, repellat sapiente odio possimus dicta, tenetur minus nam. Iure exercitationem nesciunt quaerat voluptatem ipsa ipsam, excepturi eligendi impedit sapiente. Quibusdam nostrum autem deleniti, tempora molestias temporibus debitis velit quasi harum a obcaecati placeat minima sint, sapiente necessitatibus modi eum delectus facere commodi id voluptatum eligendi recusandae.
+
+            </p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis dolores minima officia voluptatum dolorum impedit labore ipsa vero ipsum alias?
+                
+            </p>
+        </div>
+    );
+};
+
+export default About;
