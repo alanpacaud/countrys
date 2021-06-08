0982eb235e6aaec40edbc4bff85ce2d39091f315
alanpacaud <alan.propacaud@gmail.com>
2021-06-08 18:12:46 +0200

    add countries hook

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 const Countries = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag')
+        .then((res) => setData(res.data));
+    
+        //console.log(data);
+    },  []);
+    
+
     return (
-        <div>
-            
+        <div className="countries">
+            <ul className="countries-list">
+                {data.map((country) => (
+                    <li>{country.name}</li>
+                ))}
+            </ul>
         </div>
     );
 };
